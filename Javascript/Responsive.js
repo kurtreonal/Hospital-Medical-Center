1d05d50eae04f394f5189ff8acf10a94d42ff25c
Kurt Pascua <kurtreonal26@gmail.com>
2024-08-31 23:43:02 +0800

    added

---
+++ Javascript/Responsive.js
@@ -0,0 +1,20 @@
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+const navLink = document.querySelectorAll(".nav-link");
+
+hamburger.addEventListener("click", mobileMenu);
+navLink.forEach(n => n.addEventListener("click", closeMenu));
+
+function mobileMenu() {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+}
+
+function closeMenu() {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+}
+
+function submitted() {
+    alert("Appointment Submitted! Please check your email and wait for further notice.")
+}
